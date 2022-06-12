9d6642fcd34df5e045c7201f8eaec11193715494
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-06-12 18:42:52 +0400

    progress bar

--- Progress Steps/ProgressSteps.js
+++ Progress Steps/ProgressSteps.js
@@ -15,28 +15,29 @@ prev.addEventListener('click', () => {
     if (currentActive < 1) {
         currentActive = 1;
     }
- Update();
+    Update();
 
 });
 
-function Update() {
-    circles.forEach((circle, idx) => {
-        if (idx < currentActive)
-            circle.classList.add('active');
-    } )
-    else {
-        circles.classList.remove('active');
-    };
-   const.actives=document.querySelectorAll('.active');
-   console.log((actives.lenght -1)/ (circles.lenght -1)*100+ "%");
-if (currentActive ===1){
-    prev.disabled=true;
-}
-else if (currentActive===circles.lenght){
-    next.disabled=true;
-    else{
-        prev.disabled=false;
-        next.disabled=false;
+function update() {
+    circles.forEach((circle, index) => {
+      if(index < currentActive) {
+        circle.classList.add('active')
+      } else {
+        circle.classList.remove('active')
+      }
+     })
+    
+    const actives = document.querySelectorAll('.active');
+    
+    progress.style.width = (actives.length -1) / (circles.length - 1) * 100 + '%'
+    
+    if(currentActive === 1) {
+      prev.disabled = true;
+    } else if(currentActive === circles.length) {
+      next.disabled = true;
+    } else {
+      prev.disabled = false;
+      next.disabled = false;
     }
-}
-}
+  }
